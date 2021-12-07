8a9a9870984d762b5304bdf3bc9d7d26ef77fc1c
William Haley <whaley@aledade.com>
2021-12-06 21:51:44 -0500

    d1

--- D1/D1.py
+++ D1/D1.py
@@ -6,10 +6,9 @@ with open('/Users/whaley/Dev/AOC21/D1/d1_input.txt') as f:
     print(a[1])
 """
 # part 2
-count = 0
 with open('/Users/whaley/Dev/AOC21/D1/d1_input.txt') as f:
     a = [list(map(int, f.readlines())), 0]
     while len(a[0])-3:
-        count += (sum(a[0][0:3]) < sum(a[0][1:4]))
+        a[1] += (sum(a[0][0:3]) < sum(a[0][1:4]))
         a[0].pop(0)
-    print(count)
+    print(a[1])
